3558bdbc9a53fffaf7ae5c07763f9ec11a7e54e9
dave eight <sznhwlbyiatkglqpwmwfphpr465joj@gmail.com>
2021-01-05 19:04:39 +0100

    initial commit

--- qt/ts/src/reviewer.ts
+++ qt/ts/src/reviewer.ts
@@ -1,8 +1,17 @@
 /* Copyright: Ankitects Pty Ltd and contributors
  * License: GNU AGPL, version 3 or later; http://www.gnu.org/licenses/agpl.html */
 
+declare var CodeJar: any;
+declare var withLineNumbers: any;
+declare var Prism: any;
+declare var hljs: any;
+declare var console: Console;
+
 var ankiPlatform = "desktop";
 var typeans;
+var codeans;
+var log;
+var codeansJar;
 var _updatingQA = false;
 
 var qFade = 100;
@@ -38,6 +47,7 @@ function _updateQA(html, fadeTime, onupdate, onshown) {
         // update text
         try {
             qa.html(html);
+            _initalizeCodeEditor();
         } catch (err) {
             qa.html(
                 (
@@ -68,7 +78,7 @@ function _showQuestion(q, bodyclass) {
         function () {
             // return to top of window
             window.scrollTo(0, 0);
-
+            _initializeProgress()
             document.body.className = bodyclass;
         },
         function () {
@@ -81,7 +91,81 @@ function _showQuestion(q, bodyclass) {
     );
 }
 
-function _showAnswer(a, bodyclass) {
+function highlight(editor: HTMLElement) {
+    // highlight.js does not trims old tags,
+    // let's do it by this hack.
+    editor.textContent = editor.textContent;
+    hljs.highlightBlock(editor);
+}
+
+function _initializeProgress() {
+    _setProgress('0');
+}
+
+function _setProgress(raise) {
+    _displayProgressBar(raise, '#38c172')
+}
+
+function _setProgressError() {
+    _displayProgressBar('100', '#e3342f')
+}
+
+function _displayProgressBar(raise, bgColor) {
+    (<any>$('#progressbar')).jQMeter({
+        goal: '100',
+        raised: raise,
+        height: '5px',
+        barColor: bgColor,
+        bgColor:'#dadada',
+        animationSpeed: 0,
+        displayTotal: false
+    });
+}
+
+function _activateRunButton() {
+    var $runBtn = $('#start-testing');
+    var $stopBtn = $('#stop-testing');
+    $stopBtn.addClass('disabled').attr('disabled', 'disabled')
+    $runBtn.removeClass('disabled').removeAttr('disabled')
+}
+
+function _activateStopButton() {
+    var $runBtn = $('#start-testing');
+    var $stopBtn = $('#stop-testing');
+    $runBtn.addClass('disabled').attr('disabled', 'disabled')
+    $stopBtn.removeClass('disabled').removeAttr('disabled')
+}
+
+function _initalizeCodeEditor() {
+    codeans = document.getElementById("codeans");
+    if (!codeans) {
+        return;
+    }
+    log = document.getElementById("log");
+    let options = {
+        tab: " ".repeat(4), // default is '\t'
+        indentOn: /[(\[]$/, // default is /{$/
+    };
+    codeansJar = CodeJar(codeans, withLineNumbers(highlight), options);
+}
+
+function _switchSkin(name) {
+    $("head link[rel=stylesheet]").each(function () {
+        const $this = $(this);
+        let $toEnable = null;
+        if ($this.attr("href").indexOf("highlight/") > 0) {
+            if ($this.attr("href").endsWith(name + ".css")) {
+                $toEnable = $this;
+            }
+            $this.attr("disabled", "disabled");
+        }
+        if ($toEnable != null) {
+            $toEnable.removeAttr("disabled");
+        }
+    });
+}
+
+function _showAnswer(a, bodyclass, isCodingQuestion) {
     _updateQA(
         a,
         aFade,
@@ -96,9 +180,55 @@ function _showAnswer(a, bodyclass) {
             if (e[0]) {
                 e[0].scrollIntoView();
             }
+            if (isCodingQuestion) {
+                _initializeCodeAnswers();
+            }
         },
-        function () {}
+        function () { }
     );
+}
+
+function _reloadCode(src, lang) {
+    var $codeans = $(codeans);
+    $codeans.removeClass(function (index, className) {
+        return (className.match(/\blanguage-\S+/g) || []).join(" ");
+    });
+    $codeans.addClass("language-" + lang);
+    codeansJar.updateCode(src);
+}
+
+function _cleanConsoleLog() {
+    $("#log").empty();
+}
+
+function _showConsoleLog(html) {
+    const $log = $('#log')
+    $log.append(html).scrollTop($log.prop("scrollHeight"));
+}
+
+function _initializeCodeAnswers() {
+    const $qa = $('#qa')
+    const input = $qa.html()
+
+    let html = ''
+    let match
+    const regex = /```(\w+)(<br>|\\n)*([^`]+)```/g;
+    while (match = regex.exec(input)) {
+        const lang = match[1]
+        const src = match[3].replace(/<br>/g, '\n')
+        const height = src.split('\n').length
+        html += `<h4>${lang.replace(lang[0], lang[0].toUpperCase())}</h4><div class="editor language-${lang}" style="height:${height*20}px;">${src}</div><br><br>`
+    }
+    $qa.html(html)
+    $qa.find('.editor').each(function() {
+        let options = {
+            tab: ' '.repeat(4),
+            indentOn: /[(\[]$/,
+        };
+        CodeJar(this, withLineNumbers(highlight), options);
+        $(this).attr('contenteditable', 'false')
+    });
+    $(window).scrollTop(0);
 }
 
 const _flagColours = {
@@ -141,3 +271,4 @@ function _emulateMobile(enabled: boolean) {
         list.remove("mobile");
     }
 }
+
